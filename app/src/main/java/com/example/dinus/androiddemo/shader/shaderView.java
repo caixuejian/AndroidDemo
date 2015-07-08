7b6991c0fdf1755677f6df0803daa70bb4f7fb59
dinus <dinus@DinusMacBook-Pro.local>
2015-07-08 22:18:39 +0800

    add shaderview

--- app/src/main/java/com/example/dinus/androiddemo/shader/shaderView.java
+++ app/src/main/java/com/example/dinus/androiddemo/shader/shaderView.java
@@ -3,8 +3,10 @@ package com.example.dinus.androiddemo.shader;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Shader;
 import android.util.AttributeSet;
@@ -31,10 +33,19 @@ public class ShaderView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawColor(Color.GRAY);
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shader_test);
-        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        paint.setShader(bitmapShader);
-        canvas.drawRect(0, 0, 400, 400, paint);
+        int x = 200;
+        int y = 200;
+        int bitmapHeight = bitmap.getHeight();
+        int bitmapWidth = bitmap.getWidth();
+
+        canvas.drawBitmap(bitmap, x, y, paint);
+        Matrix matrix = new Matrix();
+        matrix.postScale(1, -1);
+        Bitmap mirrorBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+
+        canvas.drawBitmap(mirrorBitmap, x, y + bitmapHeight, paint);
     }
 }
